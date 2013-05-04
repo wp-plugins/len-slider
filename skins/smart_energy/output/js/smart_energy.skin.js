148ed79926c575d0f890d6ed13b5fe9c4007943a
tigusigalpa <tigusigalpa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-04 18:26:34 +0000

    LenSlider v2.0.10
    git-svn-id: https://plugins.svn.wordpress.org/len-slider/tags/2.0.10@707958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- skins/smart_energy/output/js/smart_energy.skin.js
+++ skins/smart_energy/output/js/smart_energy.skin.js
@@ -8,16 +8,22 @@ var smart_energy_lenslider_fn = function(inparr) {
         }
     );
     jQuery(".ls_sm_en_ib_manage").prepend("<div class=\"labs\"></div>");
-    jQuery(".ls_sm_en_ibanner").tabs(
-        {
-            fx:{opacity:"toggle"},
-            select: function(event, ui) {
-                jQuery(".labs").animate({'margin-left':ui.index*240}, 200);
-            },
-            show: function(event, ui) {
-                jQuery(".ls_sm_en_ibanner_overlay").hide();
-                jQuery("#ibo_"+ui.index).fadeIn(300);
-            }
+    jQuery("#ibo_0").show();
+    var lsSEObj = jQuery(".ls_sm_en_ibanner").tabs({
+        show: {
+            effect: inparr.slideshow.ls_easing_effect
+        },
+        beforeActivate: function(event, ui) {
+            jQuery(".labs").animate({marginLeft:ui.newTab.index()*240}, 200);
+            jQuery(".ls_sm_en_ibanner_overlay").hide();
+            jQuery("#ibo_"+ui.newTab.index()).fadeIn(300);
         }
-    ).tabs("rotate", inparr.slideshow.ls_autoplay_delay);
-}
+    });
+    if(parseInt(inparr.slideshow.ls_has_autoplay) > 0 && parseInt(inparr.slideshow.ls_autoplay_delay) > 0) {
+        if(inparr.slideshow.ls_autoplay_hover_pause > 0) {
+            lsSEObj.tabs("rotate", parseInt(inparr.slideshow.ls_autoplay_delay), false).tabs("hover", true, true);
+        } else {
+            lsSEObj.tabs("rotate", parseInt(inparr.slideshow.ls_autoplay_delay), true);
+        }
+    }
+};
